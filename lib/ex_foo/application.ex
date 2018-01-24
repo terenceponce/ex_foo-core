9ec54caa2b7bd57094ed522cc1e2d58303445973
Terence Ponce <terenceponce@gmail.com>
2018-01-25 00:03:15 +0800

    Add Credo for linting

--- lib/ex_foo/application.ex
+++ lib/ex_foo/application.ex
@@ -1,5 +1,9 @@
 defmodule ExFoo.Application do
+  @moduledoc """
+  Application Module
+  """
   use Application
+  alias ExFooWeb.Endpoint
 
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
@@ -12,7 +16,8 @@ defmodule ExFoo.Application do
       supervisor(ExFoo.Repo, []),
       # Start the endpoint when the application starts
       supervisor(ExFooWeb.Endpoint, []),
-      # Start your own worker by calling: ExFoo.Worker.start_link(arg1, arg2, arg3)
+      # Start your own worker by calling:
+      # ExFoo.Worker.start_link(arg1, arg2, arg3)
       # worker(ExFoo.Worker, [arg1, arg2, arg3]),
     ]
 
@@ -25,7 +30,7 @@ defmodule ExFoo.Application do
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
   def config_change(changed, _new, removed) do
-    ExFooWeb.Endpoint.config_change(changed, removed)
+    Endpoint.config_change(changed, removed)
     :ok
   end
 end
